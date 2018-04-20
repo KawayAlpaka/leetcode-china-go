97f81b939d95c63c3ac66753ba5d73dc90161305
L <my917@qq.com>
2018-04-20 17:36:56 +0800

    LRUcache

--- 高级算法/设计问题/LRU缓存机制/LRUCache.go
+++ 高级算法/设计问题/LRU缓存机制/LRUCache.go
@@ -14,30 +14,54 @@ func Constructor(capacity int) LRUCache {
 
 // Get ...
 func (cache *LRUCache) Get(key int) int {
+	// fmt.Printf("start get:key:%d \n", key)
 	if v, ok := cache.m[key]; ok {
-		l1 := len(cache.cache)
-		for i := 0; 1 < l1; i++ {
-			if cache.cache[i] == key {
-				cache.cache = append(cache.cache[:i], cache.cache[i+1:]...)
-				break
-			}
-		}
-		cache.cache = append(cache.cache, key)
+		cache.moveToTop(key)
 		return v
 	}
+	// fmt.Printf("end get:key:%d \n", key)
+	// fmt.Printf("m:%v \n", cache.m)
+	// fmt.Printf("cache:%v \n", cache.cache)
+
 	return -1
 }
 
 // Put ...
 func (cache *LRUCache) Put(key int, value int) {
-	cache.m[key] = value
-	cache.cache = append(cache.cache, key)
-	outLen := len(cache.cache) - cache.capacity
-	if outLen > 0 {
-		ks := cache.cache[:outLen]
-		cache.cache = cache.cache[outLen:]
-		for _, k := range ks {
+	// fmt.Printf("start put:key:%d,value:%d \n", key, value)
+	if _, ok := cache.m[key]; ok {
+		cache.moveToTop(key)
+	} else {
+		cache.cache = append(cache.cache, key)
+		outLen := len(cache.cache) - cache.capacity
+		if outLen > 0 {
+			// ks := cache.cache[:outLen]
+			// cache.cache = cache.cache[outLen:]
+			// for _, k := range ks {
+			// 	delete(cache.m, k)
+			// }
+			k := cache.cache[0]
+			cache.cache = cache.cache[1:]
 			delete(cache.m, k)
+			// if k != key {
+			// 	delete(cache.m, k)
+			// }
 		}
 	}
+	cache.m[key] = value
+
+	// fmt.Printf("end put:key:%d,value:%d \n", key, value)
+	// fmt.Printf("m:%v \n", cache.m)
+	// fmt.Printf("cache:%v \n", cache.cache)
+
+}
+func (cache *LRUCache) moveToTop(key int) {
+	l1 := len(cache.cache)
+	for i := 0; 1 < l1; i++ {
+		if cache.cache[i] == key {
+			cache.cache = append(cache.cache[:i], cache.cache[i+1:]...)
+			break
+		}
+	}
+	cache.cache = append(cache.cache, key)
 }
--- 高级算法/设计问题/LRU缓存机制/LRUCache_test.go
+++ 高级算法/设计问题/LRU缓存机制/LRUCache_test.go
@@ -1,74 +1,88 @@
 package lrucache
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type Case struct {
+	capacity int
+	steps    []Step
+}
+type Step struct {
 	isGet  bool
 	key    int
 	value  int
 	expect int
 }
 
-var cases1 = []Case{
-	Case{false, 1, 1, 0},
-	Case{false, 2, 2, 0},
-	Case{true, 1, 0, 1},
-	Case{false, 3, 3, 0},
-	Case{true, 2, 0, -1},
-	Case{false, 4, 4, 0},
-	Case{true, 1, 0, -1},
-	Case{true, 3, 0, 3},
-	Case{true, 4, 0, 4},
-}
-var cases2 = []Case{
-	Case{false, 2, 1, 0},
-	Case{false, 1, 1, 0},
-	Case{true, 2, 0, 1},
-	Case{false, 4, 1, 0},
-	Case{true, 1, 0, -1},
-	Case{true, 2, 0, 1},
-}
-var cases3 = []Case{
-	Case{false, 2, 1, 0},
-	Case{false, 1, 1, 0},
-	Case{false, 2, 3, 0},
-	Case{false, 4, 1, 0},
-	Case{true, 1, 0, -1},
-	Case{true, 2, 0, 3},
+var cases = []Case{
+	Case{
+		2,
+		[]Step{
+			Step{false, 1, 1, 0},
+			Step{false, 2, 2, 0},
+			Step{true, 1, 0, 1},
+			Step{false, 3, 3, 0},
+			Step{true, 2, 0, -1},
+			Step{false, 4, 4, 0},
+			Step{true, 1, 0, -1},
+			Step{true, 3, 0, 3},
+			Step{true, 4, 0, 4},
+		},
+	},
+	Case{
+		2,
+		[]Step{
+			Step{false, 2, 1, 0},
+			Step{false, 1, 1, 0},
+			Step{true, 2, 0, 1},
+			Step{false, 4, 1, 0},
+			Step{true, 1, 0, -1},
+			Step{true, 2, 0, 1},
+		},
+	},
+	Case{
+		2,
+		[]Step{
+			Step{false, 2, 1, 0},
+			Step{false, 1, 1, 0},
+			Step{false, 2, 3, 0},
+			Step{false, 4, 1, 0},
+			Step{true, 1, 0, -1},
+			Step{true, 2, 0, 3},
+		},
+	},
+	Case{
+		2,
+		[]Step{
+			Step{true, 2, 0, -1},
+			Step{false, 2, 6, 0},
+			Step{true, 1, 0, -1},
+			Step{false, 1, 5, 0},
+			Step{false, 1, 2, 0},
+			Step{true, 1, 0, 2},
+			Step{true, 2, 0, 6},
+		},
+	},
 }
 
 func TestLRUCache(t *testing.T) {
-	cache := Constructor(2)
-	for i, c := range cases1 {
-		if c.isGet {
-			result := cache.Get(c.key)
-			if result != c.expect {
-				t.Errorf("Error LRUCache ：index=%d,result=%v,expect=%v", i, result, c.expect)
+
+	for i, c := range cases {
+		fmt.Printf("case:%d \n", i)
+		cache := Constructor(c.capacity)
+		for j, s := range c.steps {
+			if s.isGet {
+				result := cache.Get(s.key)
+				if result != s.expect {
+					t.Errorf("Error LRUCache case=%d,step=%d,result=%v,expect=%v", i, j, result, s.expect)
+				}
+			} else {
+				cache.Put(s.key, s.value)
 			}
-		} else {
-			cache.Put(c.key, c.value)
 		}
+
 	}
-	cache = Constructor(2)
-	for i, c := range cases2 {
-		if c.isGet {
-			result := cache.Get(c.key)
-			if result != c.expect {
-				t.Errorf("Error LRUCache ：index=%d,result=%v,expect=%v", i, result, c.expect)
-			}
-		} else {
-			cache.Put(c.key, c.value)
-		}
-	}
-	cache = Constructor(2)
-	for i, c := range cases3 {
-		if c.isGet {
-			result := cache.Get(c.key)
-			if result != c.expect {
-				t.Errorf("Error LRUCache ：index=%d,result=%v,expect=%v", i, result, c.expect)
-			}
-		} else {
-			cache.Put(c.key, c.value)
-		}
-	}
+
 }
